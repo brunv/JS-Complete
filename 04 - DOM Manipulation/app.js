91fe90a73ce6a2cdf88f0e211cf9924580f2f5bb
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-04 19:39:06 -0300

    add the first variables to the game

--- 04 - DOM Manipulation/app.js
+++ 04 - DOM Manipulation/app.js
@@ -8,3 +8,26 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('.dice').style.display = 'none';
+
+/*
+Events: notifications that are sent to notify the code that something happene on the webpage.
+
+Examples: clicking a button, resizing windows, scrolling down or pressing a key.
+
+Event Listener: a function that performs an action based on a certain event. It waits for a specific event to happen.
+
+Message Queue: this is where all the events that happen in the browser are put. And they sit there, waiting to be processed which only happens once the Execution Stack is empty.
+*/
