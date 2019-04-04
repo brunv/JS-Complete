210299e3842532d9c765c39997924bdc4cc64702
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-04 20:46:56 -0300

    introducing events to the game

--- 04 - DOM Manipulation/app.js
+++ 04 - DOM Manipulation/app.js
@@ -9,19 +9,6 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
-
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-document.querySelector('.dice').style.display = 'none';
-
 /*
 Events: notifications that are sent to notify the code that something happene on the webpage.
 
@@ -31,3 +18,36 @@ Event Listener: a function that performs an action based on a certain event. It 
 
 Message Queue: this is where all the events that happen in the browser are put. And they sit there, waiting to be processed which only happens once the Execution Stack is empty.
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+// Reset all scores
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    // 1. Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // console.log(dice);
+    
+    // 2. Display Result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled nuber was not a 1
+
+    // document.querySelector('#current-' + activePlayer).textContent = dice;
+    // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+});
+
+
+/* Functions: */
