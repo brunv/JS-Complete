fa7d15c54afdf7adfa188953ad4960ace57d8278
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-04 22:35:54 -0300

    add game initalization function

--- 04 - DOM Manipulation/app.js
+++ 04 - DOM Manipulation/app.js
@@ -21,16 +21,7 @@ Message Queue: this is where all the events that happen in the browser are put. 
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-// Reset all scores
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
@@ -81,6 +72,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
 /* Functions */
 
 function nextPlayer()
@@ -93,4 +86,27 @@ function nextPlayer()
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
+}
+
+function init()
+{
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1!';
+    document.getElementById('name-1').textContent = 'Player 2!';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 }
