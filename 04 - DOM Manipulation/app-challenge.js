8730f8c94700027abeeddc2e1a300dbe249a8687
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-05 00:11:04 -0300

    add dom challenge part 1

--- 04 - DOM Manipulation/app-challenge.js
+++ 04 - DOM Manipulation/app-challenge.js
@@ -1,25 +1,13 @@
 /*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
-/*
-Events: notifications that are sent to notify the code that something happene on the webpage.
-
-Examples: clicking a button, resizing windows, scrolling down or pressing a key.
-
-Event Listener: a function that performs an action based on a certain event. It waits for a specific event to happen.
-
-Message Queue: this is where all the events that happen in the browser are put. And they sit there, waiting to be processed which only happens once the Execution Stack is empty.
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 
 var scores, roundScore, activePlayer, gamePlaying;
+var lastDice;
 
 init();
 
@@ -37,7 +25,13 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         diceDOM.src = 'dice-' + dice + '.png';
     
         // 3. Update the round score IF the rolled number was not a 1
-        if (dice !== 1)
+        if (dice === 6 && lastDice === 6)
+        {
+            // Player looses score
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+        }
+        else if (dice !== 1)
         {
             // Add Score
             roundScore += dice;
@@ -48,6 +42,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             // Next player
             nextPlayer();
         }
+
+        lastDice = dice;
     }
 });
 
