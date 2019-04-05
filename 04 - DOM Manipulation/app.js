9eb2489905eeb2ee60ab1bcafbd54407a8a6fefa
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-04 21:41:01 -0300

    add more functionalities to the game

--- 04 - DOM Manipulation/app.js
+++ 04 - DOM Manipulation/app.js
@@ -43,11 +43,54 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
-    // 3. Update the round score IF the rolled nuber was not a 1
-
-    // document.querySelector('#current-' + activePlayer).textContent = dice;
-    // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+    // 3. Update the round score IF the rolled number was not a 1
+    if (dice !== 1)
+    {
+        // Add Score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+    else
+    {
+        // Next player
+        nextPlayer();
+    }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
 
-/* Functions: */
+    // 1. Add current score to global score
+    scores[activePlayer] += roundScore;
+
+    // 2. Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]; 
+
+    // 3. Check if player won the game
+    if (scores[activePlayer] >= 20)
+    {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }
+    else
+    {
+        // 4. Next player's turn
+        nextPlayer();
+    }
+
+});
+
+/* Functions */
+
+function nextPlayer()
+{
+    roundScore = 0;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
