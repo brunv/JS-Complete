6684427b950a066978715b8013648ce61b01c880
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-06 20:41:34 -0300

    add bind call apply methods

---
+++ 05 - Objects and Functions/07-bind-call-apply-methods.js
@@ -0,0 +1,36 @@
+/*
+ *  Bind, Call and Apply Methods 
+ */
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal')
+        {
+            console.log('Good ' + timeOfDay + ', Ladies and Gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        }
+        else if (style === 'friendly')
+        {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+}
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+}
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+// john.presentation.apply(emily, ['friendly', 'night']);
+
+// Carrying:
+var johnFriendly = john.presentation.bind(john,'friendly');
+johnFriendly('evening');
+johnFriendly('night');
