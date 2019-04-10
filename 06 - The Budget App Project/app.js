91c3c1b882c2aa1064bb4e162a5fa89a30c1b304
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-10 15:38:40 -0300

    init budget app project

--- 06 - The Budget App Project/app.js
+++ 06 - The Budget App Project/app.js
@@ -2,42 +2,49 @@
  * The IIFE, the immediately-invoked function expression, returns immediately
  * and so it's effectively gone. But the publicTest function that we return here
  * will always have access to the x variable and the add function because a
- * closure was created here.
+ * closure was created.
  */
 
+/* BUDGET CONTROLLER */
 var budgetController = (function() {
 
-    // private
-    var x = 23;
 
-    // private
-    var add = function(a) {
-        return x + a;
-    }
-
-    // public
-    return {
-        testing: 'Public Test',
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
 })();
 
+/* UI CONTROLLER */
 var UIController = (function() {
 
     // Some code
 
 })();
 
+/* GLOBAL APP CONTROLLER */
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
-
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+        
+        // 1. Get the filled input data
+    
+        // 2. Add the item to the budget controller
+    
+        // 3. Add the item to the UI
+    
+        // 4. Calculate the budget
+    
+        // 5. Display the budget on the UI
+        
+        console.log('asdasdd');
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        
+        // 'which' property for older browsers
+        if (event.keyCode === 13 || event.which === 13)
+        {
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIController);
