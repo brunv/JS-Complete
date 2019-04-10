94d122f386b2a804de4e74ab035dac5148ad98c5
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-10 15:56:00 -0300

    add input methods

--- 06 - The Budget App Project/app.js
+++ 06 - The Budget App Project/app.js
@@ -14,17 +14,39 @@ var budgetController = (function() {
 /* UI CONTROLLER */
 var UIController = (function() {
 
-    // Some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstring: function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
 /* GLOBAL APP CONTROLLER */
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstring();
+
     var ctrlAddItem = function() {
         
         // 1. Get the filled input data
-    
+        var input = UICtrl.getInput();
+        console.log(input);
+
         // 2. Add the item to the budget controller
     
         // 3. Add the item to the UI
@@ -33,10 +55,9 @@ var controller = (function(budgetCtrl, UICtrl) {
     
         // 5. Display the budget on the UI
         
-        console.log('asdasdd');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         
