92c5ccfbfa9f956322cd9684f007ea2977696928
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-10 17:05:43 -0300

    add new item functions

--- 06 - The Budget App Project/app.js
+++ 06 - The Budget App Project/app.js
@@ -8,6 +8,58 @@
 /* BUDGET CONTROLLER */
 var budgetController = (function() {
 
+    // function constructors
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // create new id
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // push it into out data structure
+            data.allItems[type].push(newItem);
+
+            // return the new element
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -46,19 +98,20 @@ var controller = (function(budgetCtrl, UICtrl) {
         document.addEventListener('keypress', function(event) {
             
             // 'which' property for older browsers
-            if (event.keyCode === 13 || event.which === 13)
-            {
+            if (event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
             }
         });
     };
 
     var ctrlAddItem = function() {
+        var input, newItem;
         
         // 1. Get the filled input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     
         // 3. Add the item to the UI
     
