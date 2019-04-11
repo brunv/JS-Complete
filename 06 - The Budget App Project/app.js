93a444a24a2bd18f6442fcf346add0cf8f61291f
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-10 21:56:37 -0300

    add clearfield

--- 06 - The Budget App Project/app.js
+++ 06 - The Budget App Project/app.js
@@ -106,6 +106,23 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            // console.log(fields); // NodeList
+
+            // This 'Array' is the function constructor for all arrays.
+            // This will trick the slice method into thinking that we give it an array and so it will return an array
+            fieldsArr = Array.prototype.slice.call(fields);
+            // console.log(fieldsArr); // Array
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
         getDOMstring: function() {
             return DOMstrings;
         }
@@ -140,10 +157,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
+
+        // 4. Clear the fields
+        UICtrl.clearFields();
     
-        // 4. Calculate the budget
+        // 5. Calculate the budget
     
-        // 5. Display the budget on the UI
+        // 6. Display the budget on the UI
         
     };
 
