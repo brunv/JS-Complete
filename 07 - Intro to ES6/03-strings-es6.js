1f9e0fde6517d2b7ad5ad3854dc3fd264b712a64
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-13 11:50:09 -0300

    add strings in es6

---
+++ 07 - Intro to ES6/03-strings-es6.js
@@ -0,0 +1,21 @@
+/* String in ES6 */
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2016 - year;
+}
+
+// ES5:
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6: template literals
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));     // true
+console.log(n.endsWith('th'));      // true
+console.log(n.includes('oh'));      // true
+console.log(`${firstName} `.repeat(5));   
