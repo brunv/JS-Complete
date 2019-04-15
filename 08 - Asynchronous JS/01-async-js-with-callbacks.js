6881fc63f9743b676765cdb8199dac1c719dbc67
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-15 20:22:08 -0300

    add async js with callbacks

---
+++ 08 - Asynchronous JS/01-async-js-with-callbacks.js
@@ -0,0 +1,32 @@
+/* Asynchronous JavaScript with Callbacks */
+
+// In this code, the 'setTimeout()' are simulating AJAX server requests.
+
+function getRecipe() {
+    setTimeout(() => {
+        const recipeID = [345, 563, 127, 980];
+        console.log(recipeID);
+
+        setTimeout((id) => {
+            const recipe = {
+                title: 'Fresh Tomato Pasta',
+                publisher: 'Jonas'
+            };
+
+            setTimeout(publisher => {
+                const recipe2 = {
+                    title: 'Italian Pizza',
+                    publisher: 'Jake'
+                };
+
+                console.log(recipe2);
+
+            }, 1500, recipe.publisher);
+
+            console.log(`${id}: ${recipe.title}`);
+
+        }, 1500, recipeID[2]);
+    }, 1500);
+}
+
+getRecipe();
