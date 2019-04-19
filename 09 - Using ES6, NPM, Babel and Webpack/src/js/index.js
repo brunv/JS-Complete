922c0669bd6bb331ba3368d79ab3aa0bc07318b0
Bruno Vieira <bruno.augusto.vieira@gmail.com>
2019-04-19 10:36:44 -0300

    add introduction to name import and export

--- 09 - Using ES6, NPM, Babel and Webpack/src/js/index.js
+++ 09 - Using ES6, NPM, Babel and Webpack/src/js/index.js
@@ -1,6 +1,9 @@
 // Global app controller
-import num from './test';
+import str from './models/Search';
+// named export/import
+import { add as a, multiply as m, id } from "./views/searchView";
+// import * as searchView from './views/searchView';
 
-const x = 23;
-console.log(`I imported ${num} from another module AGAIN!`);
-console.log(`Variable x is ${x}`);
+console.log(`Using imported functions! ${a(id, 2)} and ${m(3, 5)}. ${str}`);
+// console.log(`Using imported functions! ${searchView.add(searchView.id, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+
